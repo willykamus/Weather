c9cf81ae27ac383c0db6282690805df074276779
William Ching <willykamus@gmail.com>
2021-12-22 11:48:12 -0500

    Initial commit

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by William Ching on 2021-12-22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
